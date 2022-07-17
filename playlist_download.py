a99fd56098119d17dddfc1fbc3135afad0fbd180
bipinrai054 <bipinrai054@gmail.com>
2022-07-17 10:45:22 +0545

    visual flares added

--- playlist_download.py
+++ playlist_download.py
@@ -4,11 +4,14 @@ import os
 p = Playlist('https://www.youtube.com/playlist?list=PLgkbRcCnThjCsHYvpExLqZEI891dFAMR2')
 
 print(f'Downloading: {p.title}')
+print ('                    ')
 
 for video in p.videos:
     #video.streams.first().download()
     output_file = video.streams.filter(only_audio=True).first().download(output_path='./songs')
     base,ext = os.path.splitext(output_file)
     new_file = base + '.mp3'
+    print(video.title) 
+    print('                             ')
     os.rename(output_file,new_file)
 
